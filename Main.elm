9ad1d38985e9d14f8927f81b7d2cc29671fd8cb4
Daniël Illouz <daniel.illouz@me.com>
2017-03-12 21:11:39 +0100

    Complete workshop part 2

--- Main.elm
+++ Main.elm
@@ -3,16 +3,35 @@ module Main exposing (..)
 import Html exposing (..)
 import Html.Attributes exposing (..)
 
+
+model =
+    { result =
+        { id = 1
+        , name = "TheSeamau5/elm-checkerboardgrid-tutorial"
+        , stars = 66
+        }
+    }
+
+
 main =
-    div [ class "content" ]
-        [ header []
-            [
-              h1 []
-              [ text "ElmHub"
-              ]
-            , span
-                [ class "tagline" ]
-                [ text "Like GitHub, but for Elm things."
+    let
+        elmHubHeader =
+            header []
+                [ h1 [] [ text "ElmHub" ]
+                , span [ class "tagline" ] [ text "Like GitHub, but for Elm things!" ]
+                ]
+        name = model.result.name
+    in
+        div [ class "content" ]
+            [ elmHubHeader
+            , ul [ class "results" ]
+                [ li []
+                    [ span [ class "star-count" ]
+                        [ text (toString model.result.stars) ]
+                      -- TODO use the model to put a link here that points to
+                      -- https://github.com/TheSeamau5/elm-checkerboardgrid-tutorial
+                      -- by prepending the "https://github.com/" part.
+                    , a [ href ("https://github.com/" ++ name) ] [ text name ]
+                    ]
                 ]
             ]
-        ]

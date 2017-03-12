c9a335728dddad5f774200822bbdd9ba46890209
Daniël Illouz <daniel.illouz@me.com>
2017-03-12 22:02:59 +0100

    Complete workshop part 3
    Implement onClick interaction that deletes list items.

--- Main.elm
+++ Main.elm
@@ -2,36 +2,65 @@ module Main exposing (..)
 
 import Html exposing (..)
 import Html.Attributes exposing (..)
-
+import Html.Events exposing (onClick)
 
 model =
-    { result =
-        { id = 1
-        , name = "TheSeamau5/elm-checkerboardgrid-tutorial"
-        , stars = 66
-        }
+    { query = "tutorial"
+    , results =
+        [ { id = 1
+          , name = "TheSeamau5/elm-checkerboardgrid-tutorial"
+          , stars = 66
+          }
+        , { id = 2
+          , name = "grzegorzbalcerek/elm-by-example"
+          , stars = 41
+          }
+        , { id = 3
+          , name = "sporto/elm-tutorial-app"
+          , stars = 35
+          }
+        , { id = 4
+          , name = "jvoigtlaender/Elm-Tutorium"
+          , stars = 10
+          }
+        , { id = 5
+          , name = "sporto/elm-tutorial-assets"
+          , stars = 7
+          }
+        ]
     }
 
+elmHubHeader =
+    header []
+        [ h1 [] [ text "ElmHub" ]
+        , span [ class "tagline" ] [ text "Like GitHub, but for Elm things!" ]
+        ]
+
+view model =
+    div [ class "content" ]
+        [ elmHubHeader
+        , ul [ class "results" ] (List.map viewSearchResult model.results)
+        ]
+
+viewSearchResult result =
+    li []
+        [ span [ class "star-count" ] [ text (toString result.stars) ]
+        , a [ href ("https://github.com/" ++ result.name), target "_blank" ]
+            [ text result.name ]
+        , button
+            [ class "hide-result", onClick { operation = "DELETE_BY_ID", data = result.id } ]
+            [ text "X" ]
+        ]
+
+update msg model =
+    if msg.operation == "DELETE_BY_ID" then
+        { model | results = List.filter (\result -> result.id /= msg.data) model.results }
+    else
+        model
 
 main =
-    let
-        elmHubHeader =
-            header []
-                [ h1 [] [ text "ElmHub" ]
-                , span [ class "tagline" ] [ text "Like GitHub, but for Elm things!" ]
-                ]
-        name = model.result.name
-    in
-        div [ class "content" ]
-            [ elmHubHeader
-            , ul [ class "results" ]
-                [ li []
-                    [ span [ class "star-count" ]
-                        [ text (toString model.result.stars) ]
-                      -- TODO use the model to put a link here that points to
-                      -- https://github.com/TheSeamau5/elm-checkerboardgrid-tutorial
-                      -- by prepending the "https://github.com/" part.
-                    , a [ href ("https://github.com/" ++ name) ] [ text name ]
-                    ]
-                ]
-            ]
+    Html.beginnerProgram
+        { view = view
+        , update = update
+        , model = model
+        }

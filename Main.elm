d9985289f42adbb6e8498f6d0b0cd6ec906a1e60
Daniël Illouz <daniel.illouz@me.com>
2017-03-12 22:41:00 +0100

    Complete workshop part 4
    Add type annotations.

--- Main.elm
+++ Main.elm
@@ -4,6 +4,26 @@ import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (onClick)
 
+type alias Model =
+    { query : String
+    , results : List SearchResult
+    }
+
+
+type alias SearchResult =
+    { id : Int
+    , name : String
+    , stars : Int
+    }
+
+
+type alias Msg =
+    { operation : String
+    , data : Int
+    }
+
+
+model : Model
 model =
     { query = "tutorial"
     , results =
@@ -30,33 +50,47 @@ model =
         ]
     }
 
+
+elmHubHeader : Html Msg
 elmHubHeader =
     header []
         [ h1 [] [ text "ElmHub" ]
         , span [ class "tagline" ] [ text "Like GitHub, but for Elm things!" ]
         ]
 
+
+view : Model -> Html Msg
 view model =
     div [ class "content" ]
         [ elmHubHeader
         , ul [ class "results" ] (List.map viewSearchResult model.results)
         ]
 
+
+viewSearchResult : SearchResult -> Html Msg
 viewSearchResult result =
     li []
         [ span [ class "star-count" ] [ text (toString result.stars) ]
         , a [ href ("https://github.com/" ++ result.name), target "_blank" ]
             [ text result.name ]
         , button
-            [ class "hide-result", onClick { operation = "DELETE_BY_ID", data = result.id } ]
+            [ class "hide-result"
+            , onClick { operation = "DELETE_BY_ID", data = result.id }
+            ]
             [ text "X" ]
         ]
 
+
+update : Msg -> Model -> Model
 update msg model =
     if msg.operation == "DELETE_BY_ID" then
-        { model | results = List.filter (\result -> result.id /= msg.data) model.results }
+        { model
+            | results =
+                List.filter (\result -> result.id /= msg.data) model.results
+        }
     else
         model
+
 
 main =
     Html.beginnerProgram
